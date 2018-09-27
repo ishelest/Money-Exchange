3a6964cdb3173c9024a31a36e8d88356844143c5
Ivan Shelest <ishelest2012@gmail.com>
2018-09-27 12:09:50 +0300

    Money-Exchange/fix: error with tests

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    const error = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    // check for positive and not too big
+    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if (currency <= 0) return {};
     
-    if (currency > 10000) return error;
-    if (currency <= 0) return result;
     let currencyTemp = currency;
+
+    // H
     const h = Math.floor(currencyTemp / 50);
     currencyTemp = currencyTemp - (h * 50);
+    // Q
     const q = Math.floor(currencyTemp / 25);
     currencyTemp = currencyTemp - (q * 25);
+    // D
     const d = Math.floor(currencyTemp / 10);
     currencyTemp = currencyTemp - (d * 10);
+    // N
     const n = Math.floor(currencyTemp / 5);
+    currencyTemp = currencyTemp - (n * 5);
+    // P
     const p = currencyTemp;
+
+    //generate result object
     let result = {};
-    if(h) result.H = h;
-    if (q) result[Q] = q;
-    if (d) result[D] = d;
-    if (n) result[N] = n;
-    if (p) result[P] = p;
-    return JSON.stringify(result);
+    if(h) result["H"] = h;
+    if(q) result["Q"] = q;
+    if(d) result["D"] = d;
+    if(n) result["N"] = n;
+    if(p) result["P"] = p;
+
+    return result;
 }
